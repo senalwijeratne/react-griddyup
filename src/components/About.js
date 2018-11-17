660dd4549febe8ac96ad7be86c9fb57ef3be3122
Senal Wijeratne <senal.ra128@gmail.com>
2018-11-17 10:19:32 +0530

    responsive grid
    To-do: convert menu button function into react function

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
 
 function About() {
   return (
     <section className="about">
-      <img src="images/queso-taco.png" alt="Yummy Taco" className="about__mockup" />
+      <img
+        src="https://uploads.codesandbox.io/uploads/user/f752eb5a-ad47-4d7b-92c4-9aaf1424ed51/SPwM-queso-taco.png"
+        alt="Yummy Taco"
+        className="about__mockup"
+      />
       <div className="about__details">
         <h2>Featured Taco</h2>
         <p>Slim Profile, easy to hold and loaded with cheese.</p>
@@ -11,7 +15,7 @@ function About() {
         <button>Learn More →</button>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
